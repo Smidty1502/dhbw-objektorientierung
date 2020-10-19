f05090f854660ae321d82d89607c07527233c256
jeremy brixner <jeremy.brixner@web.de>
2020-10-19 14:19:25 +0200

    gasfdgsdfg

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -19,7 +19,7 @@ public:
 	GameWindow()
 		: Window(800, 600)
 	{
-		set_caption("Gosu Tutorial mit Git Test_Aenderung von Davidv2");
+		set_caption("Gosu Tutorial mit Git Test_Aenderung von Davidv2, haksdljads");
 	}
 
 	// wird bis zu 60x pro Sekunde aufgerufen.
