cb68b5592f6a5818a425d345360f539808034c65
Schmidty1502 <yanni.tom@gmail.com>
2020-12-21 10:00:22 +0100

    .

--- Beispielprojekt/YaDaJe.cpp
+++ Beispielprojekt/YaDaJe.cpp
@@ -23,8 +23,10 @@ const double DT = 100.0;
 int x = 0;
 int y = 0;
 bool jmp = false;
-int timer = 0;
 
+//Timer
+int timer = 0;
+Gosu::Font font1(20);
 
 
 //Auflösung
@@ -71,61 +73,44 @@ Projektil kugel3(KugelWilli3.hitboxUnten - tempVec, 5, 1920 / 2, true);
 unique_ptr<Projektil>kugelPtr3 = make_unique<Projektil>(kugel3);
 //vector< unique_ptr<Projektil>> WilliVec{ move(kugelPtr1) };
 
-Gosu::Font font1(20);
 
 
 
 
-Vektor2d MarioBodenvec(0, 935); Gelaende MarioBoden(MarioBodenvec, 1920);
-Vektor2d Mario1vec(1056, 840); Gelaende Mario1(Mario1vec, 96);
-Vektor2d Mario2vec(1536, 792); Gelaende Mario2(Mario2vec, 96);
-Vektor2d Mario3vec(985, 880); Gelaende Mario3(Mario3vec, 32);
-Vektor2d Mario4vec(1207, 805); Gelaende Mario4(Mario4vec, 1302 - 1206);
-Vektor2d Mario5vec(1367, 800); Gelaende Mario5(Mario5vec, 1462 - 1366);
-Vektor2d Mario6vec(1681, 740); Gelaende Mario6(Mario6vec, 32);
-Vektor2d Mario7vec(1760, 690); Gelaende Mario7(Mario7vec, 160);
-Vektor2d Mario8vec(1617, 635); Gelaende Mario8(Mario8vec, 1712 - 1616);
-Vektor2d Mario9vec(1415, 580); Gelaende Mario9(Mario9vec, 1542 - 1414);
-Vektor2d Mario10vec(1243, 560); Gelaende Mario10(Mario10vec, 1306 - 1242);
-Vektor2d Mario11vec(1085, 565); Gelaende Mario11(Mario11vec, 1148 - 1084);
-Vektor2d Mario12vec(966, 535); Gelaende Mario12(Mario12vec, 997 - 965);
-Vektor2d Mario13vec(690, 655); Gelaende Mario13(Mario13vec, 849 - 689);
-Vektor2d Mario14vec(544, 605); Gelaende Mario14(Mario14vec, 639 - 543);
-Vektor2d Mario15vec(118, 570); Gelaende Mario15(Mario15vec, 437 - 117);
-Vektor2d Mario16vec(287, 510); Gelaende Mario16(Mario16vec, 32);
-Vektor2d Mario17vec(226, 455); Gelaende Mario17(Mario17vec, 32);
-Vektor2d Mario18vec(161, 400); Gelaende Mario18(Mario18vec, 32);
-Vektor2d Mario19vec(212, 340); Gelaende Mario19(Mario19vec, 32);
-Vektor2d Mario20vec(262, 280); Gelaende Mario20(Mario20vec, 32);
-Vektor2d Mario21vec(379, 275); Gelaende Mario21(Mario21vec, 32);
-Vektor2d Mario22vec(407, 215); Gelaende Mario22(Mario22vec, 143);
-Vektor2d Mario23vec(598, 235); Gelaende Mario23(Mario23vec, 894);
-Vektor2d Mario24vec(555, 170); Gelaende Mario24(Mario24vec, 32);
-Vektor2d Mario25vec(710, 165); Gelaende Mario25(Mario25vec, 32);
-Vektor2d Mario26vec(834, 155); Gelaende Mario26(Mario26vec, 32);
-Vektor2d Mario27vec(964, 140); Gelaende Mario27(Mario27vec, 128);
-Vektor2d Mario28vec(1200, 135); Gelaende Mario28(Mario28vec, 32);
-Vektor2d Mario29vec(1320, 130); Gelaende Mario29(Mario29vec, 32);
-Vektor2d Mario30vec(1425, 90); Gelaende Mario30(Mario30vec, 160);
-Vektor2d Mario31vec(1698, 145); Gelaende Mario31(Mario31vec, 1918 - 1698);
+Vektor2d MarioBodenvec(0, 935); Gelaende MarioBoden(MarioBodenvec, 1920);		Vektor2d Mario1vec(1056, 840); Gelaende Mario1(Mario1vec, 96);
+Vektor2d Mario2vec(1536, 792); Gelaende Mario2(Mario2vec, 96);					Vektor2d Mario3vec(985, 880); Gelaende Mario3(Mario3vec, 32);
+Vektor2d Mario4vec(1207, 805); Gelaende Mario4(Mario4vec, 1302 - 1206);			Vektor2d Mario5vec(1367, 800); Gelaende Mario5(Mario5vec, 1462 - 1366);
+Vektor2d Mario6vec(1681, 740); Gelaende Mario6(Mario6vec, 32);					Vektor2d Mario7vec(1760, 690); Gelaende Mario7(Mario7vec, 160);
+Vektor2d Mario8vec(1617, 635); Gelaende Mario8(Mario8vec, 1712 - 1616);			Vektor2d Mario9vec(1415, 580); Gelaende Mario9(Mario9vec, 1542 - 1414);
+Vektor2d Mario10vec(1243, 560); Gelaende Mario10(Mario10vec, 1306 - 1242);		Vektor2d Mario11vec(1085, 565); Gelaende Mario11(Mario11vec, 1148 - 1084);
+Vektor2d Mario12vec(966, 535); Gelaende Mario12(Mario12vec, 997 - 965);			Vektor2d Mario13vec(690, 655); Gelaende Mario13(Mario13vec, 849 - 689);
+Vektor2d Mario14vec(544, 605); Gelaende Mario14(Mario14vec, 639 - 543);			Vektor2d Mario15vec(118, 570); Gelaende Mario15(Mario15vec, 437 - 117);
+Vektor2d Mario16vec(287, 510); Gelaende Mario16(Mario16vec, 32);				Vektor2d Mario17vec(226, 455); Gelaende Mario17(Mario17vec, 32);
+Vektor2d Mario18vec(161, 400); Gelaende Mario18(Mario18vec, 32);				Vektor2d Mario19vec(212, 340); Gelaende Mario19(Mario19vec, 32);
+Vektor2d Mario20vec(262, 280); Gelaende Mario20(Mario20vec, 32);				Vektor2d Mario21vec(379, 275); Gelaende Mario21(Mario21vec, 32);
+Vektor2d Mario22vec(407, 215); Gelaende Mario22(Mario22vec, 143);				Vektor2d Mario23vec(598, 235); Gelaende Mario23(Mario23vec, 894);
+Vektor2d Mario24vec(555, 170); Gelaende Mario24(Mario24vec, 32);				Vektor2d Mario25vec(710, 165); Gelaende Mario25(Mario25vec, 32);
+Vektor2d Mario26vec(834, 155); Gelaende Mario26(Mario26vec, 32);				Vektor2d Mario27vec(964, 140); Gelaende Mario27(Mario27vec, 128);
+Vektor2d Mario28vec(1200, 135); Gelaende Mario28(Mario28vec, 32);				Vektor2d Mario29vec(1320, 130); Gelaende Mario29(Mario29vec, 32);
+Vektor2d Mario30vec(1425, 90); Gelaende Mario30(Mario30vec, 160);				Vektor2d Mario31vec(1698, 145); Gelaende Mario31(Mario31vec, 1918 - 1698);
 vector<Gelaende> MarioLvl{ MarioBoden, Mario1, Mario2, Mario3, Mario4, Mario5, Mario6, Mario7, Mario8, Mario9, Mario10, Mario11, Mario12, Mario13, Mario14, Mario15, Mario16, Mario17, Mario18, Mario19, Mario20, Mario21, Mario22, Mario23, Mario24, Mario25, Mario26, Mario27, Mario28, Mario29, Mario30, Mario31 };
 
-Vektor2d Mario40vec(fbreite - 100, fhoehe - 60); Gelaende Mario40(Mario40vec, 100);
-Vektor2d Mario41vec(fbreite - 100, fhoehe - 2 * 60); Gelaende Mario41(Mario41vec, 100);
-Vektor2d Mario42vec(fbreite - 100, fhoehe - 3 * 60); Gelaende Mario42(Mario42vec, 100);
-Vektor2d Mario43vec(fbreite - 100, fhoehe - 4 * 60); Gelaende Mario43(Mario43vec, 100);
-Vektor2d Mario44vec(fbreite - 100, fhoehe - 5 * 60); Gelaende Mario44(Mario44vec, 100);
-Vektor2d Mario45vec(fbreite - 100, fhoehe - 6 * 60); Gelaende Mario45(Mario45vec, 100);
-Vektor2d Mario46vec(fbreite - 100, fhoehe - 7 * 60); Gelaende Mario46(Mario46vec, 100);
-Vektor2d Mario47vec(fbreite - 100, fhoehe - 8 *60); Gelaende Mario47(Mario47vec, 100);
-Vektor2d Mario48vec(fbreite - 100, fhoehe - 9 * 60); Gelaende Mario48(Mario48vec, 100);
-Vektor2d Mario49vec(fbreite - 100, fhoehe - 10 * 60); Gelaende Mario49(Mario49vec, 100);
-Vektor2d Mario50vec(fbreite - 100, fhoehe - 11* 60); Gelaende Mario50(Mario50vec, 100);
-Vektor2d Mario51vec(fbreite - 100, fhoehe - 12* 60); Gelaende Mario51(Mario51vec, 100);
-Vektor2d Mario52vec(fbreite - 100, fhoehe - 13* 60); Gelaende Mario52(Mario52vec, 100);
-Vektor2d Mario53vec(fbreite - 100, fhoehe - 14 * 60); Gelaende Mario53(Mario53vec, 100);
-Vektor2d Mario54vec(fbreite - 100, fhoehe - 15 * 60); Gelaende Mario54(Mario54vec, 100);
-Vektor2d Mario55vec(fbreite - 100, 145); Gelaende Mario55(Mario55vec, 100);
+Vektor2d Mario40vec(fbreite - 1000, fhoehe - 60); Gelaende Mario40(Mario40vec, 100);
+Vektor2d Mario41vec(fbreite - 1000, fhoehe - 2 * 60); Gelaende Mario41(Mario41vec, 100);
+Vektor2d Mario42vec(fbreite - 1000, fhoehe - 3 * 60); Gelaende Mario42(Mario42vec, 100);
+Vektor2d Mario43vec(fbreite - 1000, fhoehe - 4 * 60); Gelaende Mario43(Mario43vec, 100);
+Vektor2d Mario44vec(fbreite - 1000, fhoehe - 5 * 60); Gelaende Mario44(Mario44vec, 100);
+Vektor2d Mario45vec(fbreite - 1000, fhoehe - 6 * 60); Gelaende Mario45(Mario45vec, 100);
+Vektor2d Mario46vec(fbreite - 1000, fhoehe - 7 * 60); Gelaende Mario46(Mario46vec, 100);
+Vektor2d Mario47vec(fbreite - 1000, fhoehe - 8 *60); Gelaende Mario47(Mario47vec, 100);
+Vektor2d Mario48vec(fbreite - 1000, fhoehe - 9 * 60); Gelaende Mario48(Mario48vec, 100);
+Vektor2d Mario49vec(fbreite - 1000, fhoehe - 10 * 60); Gelaende Mario49(Mario49vec, 100);
+Vektor2d Mario50vec(fbreite - 1000, fhoehe - 11* 60); Gelaende Mario50(Mario50vec, 100);
+Vektor2d Mario51vec(fbreite - 1000, fhoehe - 12* 60); Gelaende Mario51(Mario51vec, 100);
+Vektor2d Mario52vec(fbreite - 1000, fhoehe - 13* 60); Gelaende Mario52(Mario52vec, 100);
+Vektor2d Mario53vec(fbreite - 1000, fhoehe - 14 * 60); Gelaende Mario53(Mario53vec, 100);
+Vektor2d Mario54vec(fbreite - 1000, fhoehe - 15 * 60); Gelaende Mario54(Mario54vec, 100);
+Vektor2d Mario55vec(fbreite - 1000, 145); Gelaende Mario55(Mario55vec, 100);
 
 vector<Gelaende> MarioLv2{ MarioBoden, Mario40, Mario41, Mario42, Mario43, Mario44, Mario45, Mario46, Mario47, Mario48, Mario49, Mario50, Mario51, Mario52, Mario53, Mario54, Mario55 };
 
